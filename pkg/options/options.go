7726dc37bd7991fe9f28fd996c69232bcc006454
Cornelius Weig <cornelius.weig@tngtech.com>
2019-02-14 21:15:31 +0100

    Remove verbs option

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -2,7 +2,6 @@ package options
 
 import (
 	"github.com/corneliusweig/ketall/pkg/printer"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/genericclioptions/printers"
 )
@@ -11,7 +10,6 @@ type KetallOptions struct {
 	CfgFile         string
 	GenericCliFlags *genericclioptions.ConfigFlags
 	PrintFlags      KAPrintFlags
-	Verbs           []v1.Verbs
 }
 
 func NewCmdOptions() *KetallOptions {
@@ -26,7 +24,7 @@ type KAPrintFlags struct {
 }
 
 func (f *KAPrintFlags) ToPrinter() (printers.ResourcePrinter, error) {
-	if f.OutputFormat == nil || *f.OutputFormat == "" || *f.OutputFormat == "short" {
+	if f.OutputFormat == nil || *f.OutputFormat == "" {
 		return printer.BasicTablePrinter{}, nil
 	}
 	return f.PrintFlags.ToPrinter()
